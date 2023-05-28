01fc51614642ddb48ed0f145d0e5321253a51ec0
coldrain-f <intmain__@naver.com>
2023-05-28 19:20:47 +0900

    [Feat] Unit 등록, 수정, 삭제 다이얼로그 추가

--- src/components/admin/UnitAdmin.vue
+++ src/components/admin/UnitAdmin.vue
@@ -5,26 +5,26 @@
         <thead>
           <tr>
             <th>#</th>
-            <th>Name</th>
+            <th>Title</th>
             <th>Actions</th>
           </tr>
         </thead>
         <tbody>
-          <tr v-for="vocabulary in vocabularyList" :key="vocabulary.id">
-            <td style="width: 5%">{{ vocabulary.id }}</td>
-            <td style="width: 50%">{{ vocabulary.title }}</td>
+          <tr v-for="unit in unitList" :key="unit.id">
+            <td style="width: 5%">{{ unit.id }}</td>
+            <td style="width: 50%">{{ unit.title }}</td>
             <td style="width: 25%">
               <v-btn
                 variant="text"
                 size="small"
                 icon="mdi-trash-can-outline"
-                @click="vocaDialogControl.showDeleteDialog = true"
+                @click="unitDialogControl.showDeleteDialog = true"
               ></v-btn>
               <v-btn
                 variant="text"
                 size="small"
                 icon="mdi-pencil-outline"
-                @click="vocaDialogControl.showUpdateDialog = true"
+                @click="unitDialogControl.showUpdateDialog = true"
               ></v-btn>
             </td>
           </tr>
@@ -39,23 +39,30 @@
       <v-divider></v-divider>
     </v-card-text>
     <v-card-actions class="mt-5">
-      <v-btn variant="flat" size="default" class="w-100" color="primary">
+      <v-btn
+        variant="flat"
+        size="default"
+        class="w-100"
+        color="primary"
+        @click="unitDialogControl.showAddDialog = true"
+      >
         <v-icon start icon="mdi-plus"></v-icon>
         Add item
       </v-btn>
     </v-card-actions>
   </v-card>
-  <!-- 단어장 삭제 다이얼로그 -->
-  <v-dialog v-model="vocaDialogControl.showDeleteDialog" width="500">
+
+  <!-- 단위 삭제 다이얼로그 -->
+  <v-dialog v-model="unitDialogControl.showDeleteDialog" width="500">
     <v-card>
       <template #title>
-        <span class="noto-sans"> ※ 단어장 삭제 </span>
+        <span class="noto-sans text-error"> ※ 단위 삭제 </span>
       </template>
       <template #append>
         <v-btn
           variant="text"
           icon="mdi-close"
-          @click="vocaDialogControl.showDeleteDialog = false"
+          @click="unitDialogControl.showDeleteDialog = false"
         >
         </v-btn>
       </template>
@@ -64,19 +71,19 @@
         <v-text-field
           readonly
           label="Title"
-          model-value="단어가 읽기다 기본편"
+          model-value="Unit 01 - 일상1"
         ></v-text-field>
         <v-text-field
           readonly
-          label="Language"
-          model-value="English"
+          label="Subword"
+          model-value="40개"
         ></v-text-field>
       </v-card-text>
       <v-card-actions class="d-flex justify-center">
         <v-btn color="error" style="width: 48%"> DELETE </v-btn>
         <v-btn
           style="width: 48%"
-          @click="vocaDialogControl.showDeleteDialog = false"
+          @click="unitDialogControl.showDeleteDialog = false"
         >
           CANCEL
         </v-btn>
@@ -84,17 +91,69 @@
     </v-card>
   </v-dialog>
 
-  <!-- 단어장 수정 다이얼로그 -->
-  <v-dialog v-model="vocaDialogControl.showUpdateDialog" width="500">
+  <!-- 단위 수정 다이얼로그 -->
+  <v-dialog v-model="unitDialogControl.showUpdateDialog" width="500">
     <v-card>
-      <v-card-text>Hello World!</v-card-text>
-      <v-card-actions>
+      <template #title>
+        <span class="noto-sans text-info"> ※ 단위 수정 </span>
+      </template>
+      <template #append>
         <v-btn
-          color="primary"
-          block
-          @click="vocaDialogControl.showUpdateDialog = false"
+          variant="text"
+          icon="mdi-close"
+          @click="unitDialogControl.showUpdateDialog = false"
         >
-          Close Dialog
+        </v-btn>
+      </template>
+      <v-card-text class="mt-5">
+        <v-text-field label="No" model-value="1" readonly></v-text-field>
+        <v-text-field
+          append-inner-icon="mdi-file-document-edit-outline"
+          label="Title"
+          model-value="Unit 01 - 일상1"
+        ></v-text-field>
+        <v-text-field
+          label="Subword"
+          model-value="40개"
+          readonly
+        ></v-text-field>
+      </v-card-text>
+      <v-card-actions class="d-flex justify-center">
+        <v-btn color="info" style="width: 48%"> UPDATE </v-btn>
+        <v-btn
+          style="width: 48%"
+          @click="unitDialogControl.showUpdateDialog = false"
+        >
+          CANCEL
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+
+  <!-- 단위 등록 다이얼로그 -->
+  <v-dialog v-model="unitDialogControl.showAddDialog" width="500">
+    <v-card>
+      <template #title>
+        <span class="noto-sans text-primary"> ※ 단위 등록 </span>
+      </template>
+      <template #append>
+        <v-btn
+          variant="text"
+          icon="mdi-close"
+          @click="unitDialogControl.showAddDialog = false"
+        >
+        </v-btn>
+      </template>
+      <v-card-text class="mt-5">
+        <v-text-field label="Title"></v-text-field>
+      </v-card-text>
+      <v-card-actions class="d-flex justify-center">
+        <v-btn color="primary" style="width: 48%"> ADD </v-btn>
+        <v-btn
+          style="width: 48%"
+          @click="unitDialogControl.showAddDialog = false"
+        >
+          CANCEL
         </v-btn>
       </v-card-actions>
     </v-card>
@@ -104,15 +163,16 @@
 <script setup>
 import { reactive, ref } from "vue";
 
-// 단어장 다이얼로그 컨트롤
-const vocaDialogControl = reactive({
+// Unit 다이얼로그 컨트롤
+const unitDialogControl = reactive({
   showDeleteDialog: false,
   showUpdateDialog: false,
+  showAddDialog: false,
 });
 
-const vocabularyList = ref([
-  { id: 3, title: "단어가 읽기다 기본편", language: "English" },
-  { id: 2, title: "단어가 읽기다 실력편", language: "English" },
-  { id: 1, title: "JLPT N5", language: "Japanese" },
+const unitList = ref([
+  { id: 3, title: "Unit 03 - 학교 생활2" },
+  { id: 2, title: "Unit 02 - 학교 생활1" },
+  { id: 1, title: "Unit 01 - 일상" },
 ]);
 </script>
