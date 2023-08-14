4b8ebe9335505351454d1615693d7d7f23dd1aa5
coldrain-f <intmain__@naver.com>
2023-08-15 00:29:40 +0900

    Refactor: VocabularyService 등록 함수 TS로 변환 [#2]

--- freesia-vuetify/src/service/vocabularyServiceTypescript.ts
+++ freesia-vuetify/src/service/vocabularyServiceTypescript.ts
@@ -3,10 +3,6 @@ import type { Vocabulary } from "@/@types/vocabulary";
 import type { AxiosInstance } from "axios";
 import { $axios } from "@/common/axios";
 
-type SearchCondition = {
-  language?: string;
-};
-
 export default class VocabularyService {
   private axios: AxiosInstance;
 
@@ -14,6 +10,13 @@ export default class VocabularyService {
     this.axios = $axios;
   }
 
+  /**
+   * 단어장 검색 API
+   *
+   * @param {PageableParams}
+   * @param {searchCondition}
+   * @returns {Vocabulary[]}
+   */
   public async search(
     pageableParams: PageableParams = { page: 0, size: 3 },
     searchCondition: SearchCondition = { language: "English" },
@@ -27,4 +30,24 @@ export default class VocabularyService {
 
     return response.data;
   }
+
+  /**
+   * 단어장 등록 API
+   *
+   * @param {VocaRegistrationFormData}
+   * @returns {number} 등록 완료된 단어장의 PK
+   */
+  public async register(formData: VocaRegistrationFormData): Promise<number> {
+    const response = await this.axios.post<number>("/vocabulary", formData);
+    return response.data;
+  }
 }
+
+type SearchCondition = {
+  language?: string;
+};
+
+type VocaRegistrationFormData = {
+  title: string;
+  language: string;
+};
