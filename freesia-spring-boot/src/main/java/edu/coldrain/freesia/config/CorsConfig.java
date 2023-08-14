dce13df677ae3d28b25809fd20b0e3f861e2a516
coldrain-f <intmain__@naver.com>
2023-08-14 21:16:32 +0900

    Chore: CorsConfig에 allowedOrigin 추가

--- freesia-spring-boot/src/main/java/edu/coldrain/freesia/config/CorsConfig.java
+++ freesia-spring-boot/src/main/java/edu/coldrain/freesia/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3001")
+                .allowedOrigins("http://localhost:3001", "http://127.0.0.1:3001")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
                 .allowedHeaders("*")
                 .allowCredentials(true)
