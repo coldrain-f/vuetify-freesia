4a405a38f57e0148cfd2d53f23e4801f2c55872e
coldrain-f <intmain__@naver.com>
2023-08-15 12:33:04 +0900

    Refactor: 공통 다이얼로그 Store TS 변환 [#3]

---
+++ freesia-vuetify/src/stores/commonMessageDialog.ts
@@ -0,0 +1,29 @@
+import { defineStore } from "pinia";
+import { ref, type Ref } from "vue";
+
+export const useCommonMessageDialogStore = defineStore("commonMessageDialog", () => {
+  const showMessageDialog: Ref<boolean> = ref(false);
+  const messageDialogText: Ref<string> = ref("");
+
+  /** 미사용 - 나중에 제거 필요 */
+  const showCommonMessageDialog = (message: string) => {
+    showMessageDialog.value = true;
+    messageDialogText.value = message;
+  };
+
+  const resetMessageDialog = (): void => {
+    showMessageDialog.value = false;
+    messageDialogText.value = "";
+  };
+
+  const VDialog = (message?: string) => {
+    resetMessageDialog();
+    showMessageDialog.value = true;
+    messageDialogText.value = message || "";
+  };
+
+  return {
+    showCommonMessageDialog, // 미사용 - 나중에 제거 필요
+    VDialog,
+  };
+});
--- freesia-vuetify/vite.config.ts
+++ freesia-vuetify/vite.config.ts
@@ -1,39 +1,39 @@
 // Plugins
-import vue from '@vitejs/plugin-vue'
-import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
+import vue from "@vitejs/plugin-vue";
+import vuetify, { transformAssetUrls } from "vite-plugin-vuetify";
 
 // Utilities
-import { defineConfig } from 'vite'
-import { fileURLToPath } from 'node:url'
-import { resolve } from 'node:path'
+import { defineConfig } from "vite";
+import { fileURLToPath } from "node:url";
+import { resolve } from "node:path";
 
 // https://vitejs.dev/config/
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = resolve(__filename, '..')
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = resolve(__filename, "..");
 
 export default defineConfig({
   plugins: [
     vue({
-      template: { transformAssetUrls }
+      template: { transformAssetUrls },
     }),
     // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
     vuetify({
       autoImport: true,
       styles: {
-        configFile: 'src/styles/settings.scss'
-      }
-    })
+        configFile: "src/styles/settings.scss",
+      },
+    }),
   ],
-  define: { 'process.env': {} },
+  define: { "process.env": {} },
   resolve: {
     alias: {
       // "@": fileURLToPath(new URL("./src", import.meta.url)),
-      '@': resolve(__dirname, 'src')
+      "@": resolve(__dirname, "src"),
     },
-    extensions: ['.js', '.json', '.jsx', '.mjs', '.ts', '.tsx', '.vue']
+    extensions: [".js", ".json", ".jsx", ".mjs", ".ts", ".tsx", ".vue", ".d.ts"],
   },
   server: {
-    port: 3001
-  }
-})
+    port: 3001,
+  },
+});
